797dca6af57c62e8530b3720f9f20dfd8504381d
Dean Wang <deanalexwang@gmail.com>
2026-05-24 21:52:17 -0400

    Actually, gimmi a sec to figure out mana
    Removed playerMana property and its backing field.

--- Assets/Scripts/CurrentData.cs
+++ Assets/Scripts/CurrentData.cs
@@ -40,19 +40,6 @@ public class CurrentData : MonoBehaviour
             }
         }
     }
-    private int m_playerMana = 50;
-    public int playerMana
-    {
-        get { return m_playerMana; }
-        set
-        {
-            if (value >= 0 && value <= 50)
-            {
-                m_playerMana = value;
-                OnPlayerManaChange?.Invoke(m_playerMana);
-            }
-        }
-    }
     void Awake()
     {
         if(Instance != null)
