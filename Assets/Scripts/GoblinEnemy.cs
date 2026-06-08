1ede52687d85d0dd6dfd86d3a520dd33a9db86e1
Monster-Mimi <thomas.ming.marshall@gmail.com>
2026-06-07 23:11:28 -0400

    The full goblin code in all its glory

--- Assets/Scripts/GoblinEnemy.cs
+++ Assets/Scripts/GoblinEnemy.cs
@@ -1,6 +1,7 @@
+using System.Collections;
 using UnityEngine;
 
-public class GoblinEnemy : BasicEnemy
+public class GoblinEnemy : BasicEnemy, IDamageable
 {
     private bool isPlayerClose = false;
     private bool isPlayerInAttackRange = false;
@@ -12,6 +13,11 @@ public class GoblinEnemy : BasicEnemy
     private const float goblinSpeed = 4;
     private Rigidbody2D goblinRigidBody;
     [SerializeField] private GameObject attackVisual;
+    private int goblinAttackCooldown = 2;
+    private float goblinTimeTillNextAttack = 0;
+    private Vector2 hitBoxRadius = new Vector2(1.5f,1.5f);
+    private Vector2 attackPoint;
+    private int hp = 35;
     void Start()
     {
         goblinRigidBody = GetComponent<Rigidbody2D>();
@@ -25,6 +31,11 @@ public class GoblinEnemy : BasicEnemy
         {
             isPlayerInAttackRange = Physics2D.OverlapCircle(gameObject.transform.position, attackRadius, playerLayer);
             Debug.Log(isPlayerInAttackRange);
+        }
+        if(hp <= 0)
+        {
+            Destroy(gameObject);
+            gainMoney(10);
         }
     }
     void FixedUpdate()
@@ -49,6 +60,41 @@ public class GoblinEnemy : BasicEnemy
         else
         {
             goblinRigidBody.linearVelocityX = 0;
+            if(goblinTimeTillNextAttack < Time.time)
+            {
+                goblinTimeTillNextAttack = Time.time + goblinAttackCooldown;
+                PreformAttack();
+            }
         }
     }
+    void PreformAttack()
+    {
+        Vector2 finalAttackPosition = transform.position;
+        if(player.position.x < transform.position.x)
+        {
+            finalAttackPosition += Vector2.left;
+        }
+        else
+        {
+            finalAttackPosition += Vector2.right;
+        }
+        Collider2D hitPlayer = Physics2D.OverlapBox(finalAttackPosition, hitBoxRadius, 0, playerLayer);
+        StartCoroutine(Attack(finalAttackPosition));
+        if (hitPlayer)
+        {
+            dealDamage(1);
+        }
+    }
+    IEnumerator Attack(Vector2 attackPosition)
+    {
+        attackVisual.transform.position = attackPosition;
+        attackVisual.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        attackVisual.SetActive(false);
+    }
+    public void TakeDamage(int damage)
+    {
+        hp -= damage;
+    }
 }
+
